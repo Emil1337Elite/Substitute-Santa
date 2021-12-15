142a2e7d123c0ef4b60ec68475ebd31e568b648f
Emil1337Elite <emiloster.katt@gmail.com>
2021-12-15 16:40:04 +0100

    uppgift 2 försök

--- substitutesanta.py
+++ substitutesanta.py
@@ -32,26 +32,37 @@ def list_r():
         for i in f:
             print(i.strip())
 
-"""def list_n():
+def list_n():
     a = input("Would you like to add names or read the naughty list.").lower()
-    cn = input("What names do yo want to add? ")
+    i = []
+    i.append(input("What names do yo want to add? "))
     if a == "add" or a == "add names":
-        with open("kolbarn.txt", "a", encoding="utf-8") as f:
-            f.write("kolbarn.txt")
-            f.writelines(cn+"\n")
-    elif a"""
+        while True:
+            with open("kolbarn.txt", "a", encoding="utf-8"):
+                a = input("Do you want to add another name? ").lower()
+                if a == "yes" or a == "y":
+                    i.append(input("What did you forget to add? \n"))
+                elif a == "no" or "n":
+                    break
+    elif a == "read":
+        with open("kolbarn.txt", "r", encoding="utf-8") as f:
+            f.readline()
+            for i in f:
+                print(i.strip())
 
 print("""Santa has gotten sick and needs help with his work so he can take time off an recover in time for Christmas. 
 You have been tasked with creating and reading the wish lists of children.""")
 def main():
     while True:
         print("Would you like to create or read a list? ")
-        print("Type 'c' to create a new file, type 'r' to read a file.")
+        print("Type 'c' to create a new file, type 'r' to read a file or type 'n' to read the naughty list.")
         a = input()
         if a == "c":
             list_c()
         elif a == "r":
             list_r()
+        elif a == "n":
+            list_n()
         else:
             print("Input not recognised.")
 
